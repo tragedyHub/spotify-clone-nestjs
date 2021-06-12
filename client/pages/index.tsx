e10505f76d47ab91684d6dc05e656c5cb70de1d8
Ivan Proskurnin <43935004+tragedyHub@users.noreply.github.com>
2021-06-13 01:59:38 +0300

    add navbar

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import {Button} from "@material-ui/core"
+import Navbar from "../components/Navbar"
 const Index = () => {
 
     return ( 
         <>
+            <Navbar />
             <div className="center">
                 <h1>Добро пожаловать!</h1>
                 <h3>Здесь собраны лучшие треки!</h3>
             </div>
+
+            <style jsx>
+                {`
+                    .center {
+                        margin-top: 150px;
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                        justify-content: center;
+                    }
+                `}
+            </style>
         </>
     )
 }
