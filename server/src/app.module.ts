be9bfb6d3eaf840ae1243664dfd0c17611d4e2ca
Ivan Proskurnin <43935004+tragedyHub@users.noreply.github.com>
2021-06-12 05:11:52 +0300

    tracks template

---
+++ server/src/app.module.ts
@@ -0,0 +1,9 @@
+import {Module} from "@nestjs/common";
+import { TrackModule } from './track/track.module';
+
+@Module({
+    imports: [TrackModule]
+})
+export class AppModule {
+
+}
---
+++ server/src/track/track.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get } from "@nestjs/common";
+
+
+@Controller('/tracks')
+export class TrackController {
+    create() {
+
+    }
+    getAll() {
+
+    }
+    getOne() {
+
+    }
+
+    delete() {
+        
+    }
+}
